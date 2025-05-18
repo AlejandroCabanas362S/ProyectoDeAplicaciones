e989ebeb6fb6f1a97d10adf714346efad23fa10f
AlejandroCuquejo <alejandro.cuquejor362@gmail.com>
2025-05-18 13:48:05 -0300

    se modifca proyecto

---
+++ Login.js
@@ -0,0 +1,17 @@
+// lista blanca de correos admitidos
+const allowedUsers = ['acabanas@gmail.com', 'lberino@gmail.com'];
+const correctPin   = '1234';
+
+document.getElementById('loginForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value.trim().toLowerCase();
+  const pin   = document.getElementById('password').value.trim();
+
+  if (allowedUsers.includes(email) && pin === correctPin) {
+    // login válido
+    window.location.href = 'index.html';
+  } else {
+    alert('Credenciales inválidas');
+  }
+});
